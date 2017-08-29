f99fc93e20bbd96c48ff23bf1e2028ca73815209
wellRich <kitty_gyk@163.com>
2017-08-29 23:42:49 +0800

    first commit

---
+++ sprMVC/src/test/java/gyk/BeanTest.java
@@ -0,0 +1,12 @@
+package gyk;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanTest {
+    public static void main(String[]args){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+        Person person = ctx.getBean("person", Person.class);
+        person.useAxe();
+    }
+}
